e0edc17d666d784e7404c68242815077257e8250
Pranjal Rajput <pranjal.rajput.che21@itbhu.ac.in>
2023-06-08 00:46:22 +0530

    Created The Navbar

---
+++ src/Components/Navbar/navbar.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import "./navbar.css";
+const Navbar = () => {
+	return (
+		<div className="mainNav">
+			<div className="header">
+				<div className="navLogo">Coding Profile</div>
+				<nav>
+					<ul className="nav__links">
+						<li>
+							<NavLink to="/">Profile</NavLink>
+						</li>
+						<li>
+							<NavLink to="/Contests">Contests</NavLink>
+						</li>
+						<li>
+							<NavLink to="/Questions">Questions</NavLink>
+						</li>
+					</ul>
+				</nav>
+			</div>
+		</div>
+	);
+};
+
+export default Navbar;
