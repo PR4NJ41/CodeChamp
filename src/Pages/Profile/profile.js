604671bdf546dc36b6131d2dd94b7da3f88db9dd
Pranjal Rajput <pranjal.rajput.che21@itbhu.ac.in>
2023-06-08 00:51:31 +0530

    Created Profile and Other Pages

---
+++ src/Pages/Profile/profile.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Navbar from "../../Components/Navbar/navbar";
+
+const profile = () => {
+	return (
+		<div className="main">
+			<Navbar />
+		</div>
+	);
+};
+
+export default profile;
