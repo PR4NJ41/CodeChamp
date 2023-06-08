1369346629a0493ffac12254abedbda4edb0405e
Pranjal Rajput <pranjal.rajput.che21@itbhu.ac.in>
2023-06-08 15:38:19 +0530

    Further UI Changes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Profile from "./Pages/Profile/profile";
 import Questions from "./Pages/Questions/questionsPage";
 import Contests from "./Pages/Contests/contestsPage";
+import Test from "./Pages/test/test";
 
 function App() {
 	return (
@@ -10,6 +11,7 @@ function App() {
 				<Route path="/" element={<Profile />} />
 				<Route path="questions" element={<Questions />} />
 				<Route path="contests" element={<Contests />} />
+				<Route path="test" element={<Test />} />
 			</Routes>
 		</BrowserRouter>
 	);
