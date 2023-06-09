e0987ad43871a45d27ebce59b54cc4cdd324d98a
Pranjal Rajput <pranjal.rajput.che21@itbhu.ac.in>
2023-06-09 22:46:50 +0530

    Added Some Components in Profile PAge

--- src/Components/Context/context.js
+++ src/Components/Context/context.js
@@ -2,37 +2,44 @@ import React, { useEffect, useState, useContext } from "react";
 
 const contestApi = "https://codeforces.com/api/contest.list";
 const questionApi = "https://codeforces.com/api/problemset.problems";
-const submissionApi =
-	"https://codeforces.com/api/user.status?handle=unseenSpirit";
 
 const AppContext = React.createContext(0);
 const AppProvider = ({ children }) => {
 	const [contests, setContests] = useState([]);
 	const [questions, setQuestions] = useState([]);
-	const [isLoading, setIsLoading] = useState(true);
-	const [qLoading, setQLoading] = useState(true);
-
 	const [submissions, setSubmissions] = useState([]);
 	const [acceptedProblems, setAcceptedProblems] = useState([]);
+	const [userName, setUserName] = useState("");
+	const [newQuestions, setNewQuestions] = useState(questions);
+	const [profile, setProfile] = useState([]);
 
 	const getContests = async (url) => {
 		try {
 			const res = await fetch(url);
 			const data = await res.json();
-			const p = data.result;
-			console.log(p);
-			setContests(p);
-			setIsLoading(false);
+			setContests(data.result);
 		} catch (error) {
 			console.log(error);
 		}
 	};
+	let fQ = [];
+	const [i, setI] = useState(0);
+
 	const getQuestions = async (url) => {
 		try {
 			const response = await fetch(url);
 			const data = await response.json();
-			setQuestions(data.result.problems);
-			setQLoading(false);
+			// data.result.problems.map((q) => {
+			// 	{
+			// 		q.rating && fQ.push(q);
+			// 	}
+			// });
+			fQ = data.result.problems.filter((q) => q.rating > 0);
+
+			setQuestions(fQ);
+
+			// k = data.result.problems.filter((f) => f.rating != null);
+			setNewQuestions(fQ);
 		} catch (error) {
 			console.log(error);
 		}
@@ -44,15 +51,45 @@ const AppProvider = ({ children }) => {
 			const data = await response.json();
 			setSubmissions(data.result);
 		} catch (error) {
-			console.error(console.log(error));
+			console.log(error);
+		}
+	};
+	const fetchProfile = async (name) => {
+		try {
+			const response = await fetch(
+				`https://codeforces.com/api/user.info?handles=${name}`
+			);
+			const data = await response.json();
+			setProfile(data.result[0]);
+			console.log(data.result[0]);
+		} catch (error) {
+			console.log(error);
 		}
 	};
 
 	useEffect(() => {
+		const m = localStorage.getItem("user");
+		console.log(localStorage.getItem("user"), "name");
+		fetchProfile(m);
+		console.log(profile);
+		setUserName(localStorage.getItem("user"));
 		getContests(contestApi);
 		getQuestions(questionApi);
-		fetchSubmissions(submissionApi);
+
+		const timer = setTimeout(() => {}, 4000);
+
+		return () => clearTimeout(timer);
 	}, []);
+
+	useEffect(() => {
+		if (localStorage.getItem("user")) {
+			fetchSubmissions(
+				`https://codeforces.com/api/user.status?handle=${localStorage.getItem(
+					"user"
+				)}`
+			);
+		}
+	}, [userName]);
 
 	useEffect(() => {
 		const acceptedProblems = submissions
@@ -67,7 +104,21 @@ const AppProvider = ({ children }) => {
 
 	return (
 		<AppContext.Provider
-			value={{ isLoading, contests, questions, acceptedProblems }}
+			value={{
+				contests,
+				questions,
+				userName,
+				submissions,
+				acceptedProblems,
+				setUserName,
+				setSubmissions,
+				setAcceptedProblems,
+				newQuestions,
+				setNewQuestions,
+				i,
+				setI,
+				profile,
+			}}
 		>
 			{children}
 		</AppContext.Provider>
--- src/Pages/Profile/profile.js
+++ src/Pages/Profile/profile.js
@@ -1,12 +1,77 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Navbar from "../../Components/Navbar/navbar";
+import "./profile.css";
+import { useGlobalContext } from "../../Components/Context/context";
 
-const profile = () => {
+const Profile = () => {
+	const { userName, setUserName, acceptedProblems, profile } =
+		useGlobalContext();
+
 	return (
 		<div className="main">
 			<Navbar />
+			<div className="profile">
+				<input
+					placeholder={acceptedProblems.length}
+					id="userNameInput"
+				></input>
+				<button
+					onClick={() => {
+						localStorage.setItem(
+							"user",
+							document.getElementById("userNameInput").value
+						);
+						setUserName(
+							document.getElementById("userNameInput").value
+						);
+						window.location.reload();
+					}}
+				>
+					Save
+				</button>
+				<div className="userData">
+					<div className="left">
+						<img src={profile.titlePhoto} className="userImg"></img>
+					</div>
+					<div className="right">
+						<table>
+							<tr>
+								<td>Name</td>
+								<td>
+									{profile.firstName} {profile.lastName}
+								</td>
+							</tr>
+
+							<tr>
+								<td>Country</td>
+								<td>{profile.country}</td>
+							</tr>
+							<tr>
+								<td>Rating</td>
+								<td>{profile.rating}</td>
+							</tr>
+							<tr>
+								<td>Handle</td>
+								<td>{profile.handle}</td>
+							</tr>
+							<tr>
+								<td>Rank</td>
+								<td>{profile.rank}</td>
+							</tr>
+							<tr>
+								<td>Maximum Rating</td>
+								<td>{profile.maxRating}</td>
+							</tr>
+							<tr>
+								<td>Maximum Rank</td>
+								<td>{profile.maxRank}</td>
+							</tr>
+						</table>
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 };
 
-export default profile;
+export default Profile;
